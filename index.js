f7912770287be32daf6117f78c4cc1d133e6b153
Anesu <murac96@gmail.com>
2021-09-05 10:00:01 +0200

    Added db schema and connection details

--- index.js
+++ index.js
@@ -31,3 +31,17 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
