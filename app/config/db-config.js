5301eab1d656f649792bb853bcae33771ad34b40
Anesu <murac96@gmail.com>
2021-09-22 11:56:13 +0200

    Updated new db url

--- app/config/db-config.js
+++ app/config/db-config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://e5842dbc-285b-6:dc8db980-39e6-f@data-capsule-xcquif.codecapsules.co.za:27017/app?ssl=true&authSource=admin"
+  url: "mongodb://9097b31f-2edd-e:664d9a9c-56bf-f@data-capsule-pcmbfb.codecapsules.co.za:27017/app?ssl=true&authSource=admin"
 };
